0feed8a21817cdf8faed40278dbf8556ee9f00b4
Akanni Silva <akanni029@gmail.com>
2025-08-22 14:31:09 -0300

    primeira atividade

---
+++ Atividade1/main.js
@@ -0,0 +1,7 @@
+const leia = require('readline-sync');
+
+let salario = leia.questionFloat('Digite o salario do funcionario: ' );
+let abono = leia.questionFloat('Digite o valor do abono: ');
+let novoSalario = salario + abono;
+
+console.log(`salario atual:` + novoSalario)
